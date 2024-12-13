756063c7912716d203a315448859fd1886d6ba37
MCHP-Andy <andy.chang@microchip.com>
2024-12-13 17:00:37 +0800

    feat(tools): build project by cmd

--- Sample/KF_loopback_with_Randy_wka_v1/gen.bat
+++ Sample/KF_loopback_with_Randy_wka_v1/gen.bat
@@ -4,8 +4,10 @@
 
 @REM echo %PATH%
 
+set MAKE_TOOL_PATH="C:\Program Files\Microchip\MPLABX\v6.20\gnuBins\GnuWin32\bin"
 set OBJ_TOOL_PATH=..\..\zephyr-sdk-0.17.0\arm-zephyr-eabi\bin
 set IMG_TOOL_PATH=..\..\CPGZephyrDocs\MEC175x
+set PROG_TOOL_PATH="C:\Program Files (x86)\DediProg\SF Programmer"
 @REM set PATH=%PATH%;%IMG_TOOL_PATH%;%IMG_TOOL_PATH%
 
 
@@ -20,6 +22,18 @@ set IMG_TOOL_PATH=..\..\CPGZephyrDocs\MEC175x
 
 @REM 
 
+echo "Clean pre-files"
+del zephyr.bin
+del spi_image.bin
+
+
+echo "Build with Make"
+cd kf_freertos_fpga.X
+@REM "%MAKE_TOOL_PATH%"\make clean
+%MAKE_TOOL_PATH%\make
+cd ..
+
+
 
 echo "Generate bin file from elf"
 %OBJ_TOOL_PATH%\arm-zephyr-eabi-objcopy.exe --input-target=ihex --output-target=binary kf_freertos_fpga.X\dist\kf_freertos_fpga\production\kf_freertos_fpga.X.production.hex zephyr.bin
@@ -31,6 +45,5 @@ echo "Generate SPI Image"
 %IMG_TOOL_PATH%\mec175x_spi_gen.exe -i mec175x_spi_cfg.txt -o spi_image.bin
 
 echo "Flash SPI Image"
-set PROG_TOOL_PATH="C:\Program Files (x86)\DediProg\SF Programmer"
 @REM dediprog "--spi-image=${PROJECT_BINARY_DIR}/${SPI_IMAGE_NAME}" "--vcc=0"
 %PROG_TOOL_PATH%\DpCmd.exe -uspi_image.bin
